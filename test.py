1561b99853db0edbfd8ad324377934435b432a09
Hsueh-Ti (Derek) Liu <htliu1992@gmail.com>
2017-10-14 02:06:12 -0400

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from readOBJ import *
-from vertArea import *
-from cotanLaplace import *
-from adjacencyMat import *
-
-V,F = readOBJ('./meshes/fandisk.obj')
-VA = vertArea(V, F)
-L = cotanLaplace(V,F)
-A = adjacencyMat(F)
