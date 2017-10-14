8c58deef9b200218b90f9ba8270379d6e0136622
HTDerekLiu <htliu1992@gmail.com>
2017-10-14 17:34:59 -0400

    test scripts

--- test.py
+++ test.py
@@ -2,8 +2,28 @@ from readOBJ import *
 from vertArea import *
 from cotanLaplace import *
 from adjacencyMat import *
+from plotTriMesh import *
 
 V,F = readOBJ('./meshes/fandisk.obj')
 VA = vertArea(V, F)
 L = cotanLaplace(V,F)
 A = adjacencyMat(F)
+
+##from mpl_toolkits.mplot3d import Axes3D
+##from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+##
+##
+##import matplotlib.pyplot as plt
+##fig = plt.figure(figsize=plt.figaspect(0.5))
+##ax = fig.add_subplot(1, 2, 1, projection='3d')
+##ax.plot_trisurf(V[:,0], V[:,1], V[:,2], triangles=F)
+##plt.show()
+
+# from mayavi import mlab
+# mlab.clf()
+# mlab.triangular_mesh(V[:,0], V[:,1], V[:,2], F)
+# mlab.show()
+
+C = np.zeros([F.shape[0], 1])
+C[1] = 1
+plotTriMesh(V,F, faceColor = C, opacity=0.1)
