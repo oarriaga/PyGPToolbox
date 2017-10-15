04e34b85b90ff69a723382a72498f1638cd340b5
HTDerekLiu <htliu1992@gmail.com>
2017-10-14 21:33:53 -0400

    add demo

--- demo_visualizeLaplace.py
+++ demo_visualizeLaplace.py
@@ -1,17 +1,15 @@
-from readOBJ import *
-from vertArea import *
-from cotanLaplace import *
-from adjacencyMat import *
-from plotTriMesh import *
-#from mayavi import mlab
 import scipy
 import scipy.sparse
 import scipy.sparse.linalg
 
+from readOBJ import *
+from vertArea import *
+from cotanLaplace import *
+from plotTriMesh import *
+
 V,F = readOBJ('./meshes/spot.obj')
 VA = vertArea(V, F)
 L = cotanLaplace(V,F)
-A = adjacencyMat(F)
 
 massMat = scipy.sparse.coo_matrix(np.diag(VA)).tocsr()
 vals, vecs = scipy.sparse.linalg.eigs(L, M=massMat, k=10, which='SM')
@@ -19,5 +17,3 @@ vals = np.real(vals)
 vecs = np.real(vecs)
 
 plotTriMesh(V,F, vertexColor = vecs[:,0])
-plotTriMesh(V,F, vertexColor = vecs[:,1])
-plotTriMesh(V,F, vertexColor = vecs[:,2])
